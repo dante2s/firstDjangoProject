e1379686e8f8d1e4062f03121b3e4d4c192e23b4
dmitry.k <dmitry.k@nimax.agency>
2023-11-10 10:38:26 +0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Главная</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Наш клуб</h1>')
