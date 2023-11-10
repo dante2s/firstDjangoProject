cbc88b3422f92792621b3b719628fb88f276ca29
dmitry.k <dmitry.k@nimax.agency>
2023-11-10 11:31:48 +0300

    added posts

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
+from blog.models import Post
 
 
 def home(request):
-    return HttpResponse('<h1>Главная</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Наш клуб</h1>')
+    return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
